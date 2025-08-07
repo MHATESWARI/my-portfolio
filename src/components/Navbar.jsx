b4d2dc25105bc91e37849ec304a74f260dcd6237
Maadhu <mhateswari@gmail.com>
2025-08-07 23:58:21 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import myPic from '../assets/myPic.jpg';
+import React from 'react'
+import myPic from '../assets/myPic.jpg'
 
 const Navbar = () => {
   return (
-    <div className='bg-gradient-to-r from-teal-500 to-teal-100'>
+    <div className=' bg-gradient-to-r from-teal-500 to-teal-100'>
 
       {/* Fixed Navbar */}
       <div className='fixed top-0 z-50 w-full bg-gradient-to-r from-teal-500 to-teal-100 shadow-md'>
-        <div className='flex flex-col lg:flex-row items-center justify-between px-4 lg:px-16 py-4 lg:py-6'>
+        <div className='flex items-center py-6'>
 
           {/* Name and Role */}
-          <div className='text-center lg:text-left'>
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-serif text-slate-700 tracking-wider uppercase drop-shadow-sm">
+          <div className='font-bold text-center ml-60'>
+            <h1 className="text-5xl font-serif text-slate-700 tracking-wider uppercase drop-shadow-sm">
               MHATESWARI. K
             </h1>
-            <p className="text-sm sm:text-base text-slate-600 mt-2">
+            <p className="text-base text-slate-600 mt-2">
               Frontend Developer &nbsp; | &nbsp; React JS &nbsp; | &nbsp; Passion for Clean UI
             </p>
           </div>
 
           {/* Navigation Menu */}
-          <div className='flex space-x-6 sm:space-x-10 mt-4 lg:mt-0 text-sm sm:text-lg text-slate-600 font-bold'>
+          <div className='flex space-x-10 ml-80 text-lg text-slate-600 font-bold mt-6'>
             <p className='relative cursor-pointer group'>
               ABOUT
               <span className='absolute left-0 -bottom-1 h-[2px] w-0 bg-slate-800 transition-all duration-300 group-hover:w-full'></span>
@@ -39,7 +39,7 @@ const Navbar = () => {
       </div>
 
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
